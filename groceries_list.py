9c75cf55abf728f4b20775b3a5190e53548e5fa0
Daltoniana <danyaramirezgomez@gmail.com>
2016-10-15 19:31:31 -0400

    Fix rules bug and abstract out file location

--- groceries_list.py
+++ groceries_list.py
@@ -1,8 +1,12 @@
 import openpyxl #to be able to work with Excel
 import csv #to be able to work with .csv
+import os #to be able to find the files in the current folder
+
+#Location of THIS file is dir_path
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 #Accessed the cells in meal plan
-document = openpyxl.load_workbook("/Users/danyaramirez/Dropbox/Shared_Kyle_Danya/Food/Program/meal_plan.xlsx")
+document = openpyxl.load_workbook(dir_path + "/meal_plan.xlsx")
 sheet =  document.get_sheet_by_name("current week")
 many_cells = sheet["B1":"H47"]
 
@@ -15,7 +19,7 @@ for row in many_cells:
 rr = [x for x in all_foods if x is not None]
 
 #Accessing the dictionary for amounts.
-reader = csv.reader(open("/Users/danyaramirez/Dropbox/Shared_Kyle_Danya/Food/Program/meal_amounts.csv","r"))
+reader = csv.reader(open(dir_path + "/meal_amounts.csv","Ur"))
 d = {}#creating the dictionary
 for row in reader:#telling python that in the file above the elements in a and b are a row,and that b is th evalue of the key a.
     a,b = row
@@ -37,21 +41,30 @@ for item in instances_of_food:
     total_per_food[item] = float(total)
 
 #Accessing the dictionary for foods and units.
-reader = csv.reader(open("/Users/danyaramirez/Dropbox/Shared_Kyle_Danya/Food/Program/meal_units.csv","r"))
+reader = csv.reader(open(dir_path + "/meal_units.csv","Ur"))
 units = {}#creating the dictionary
-for row in reader:#telling python that in the file above the elements in a and b are a row,and that b is th evalue of the key a.
+for row in reader: #telling python that in the file above the elements in a and b are a row,and that b is the evalue of the key a.
     a,b = row
     units[a] = b
+
+#Accessing the dictionary for shopping rules.
+reader = csv.reader(open(dir_path + "/shopping_rules.csv","Ur"))
+rules = {} #creating the dictionary
+for row in reader: #telling python that in the file above the elements in a and b are a row,and that b is the evalue of the key a.
+    a,b = row
+    rules[a] = b
 
 #Creating a final list of foods
 final_list = set(rr)
 final_ordered = sorted(final_list)
 # print final_ordered
 
-#Printing one by one [foods, amounts and units] and export to .txt
-file = open("/Users/danyaramirez/Dropbox/Shared_Kyle_Danya/Food/Program/shopping_list.txt","w")
+#Printing one by one [foods, amounts, units and rules] and export to .txt
+file = open(dir_path + "/shopping_list.txt","w")
 for item in final_ordered:
     if item in units:
-        print>>file, item + " - " + str(total_per_food[item])+ units[item]
+        print>>file, item + " - " + str(total_per_food[item])+ units[item] + "\n"
+        if item in rules:
+            print>>file, "\t" + rules[item] + "\n"
     else:
-        print>>file,str(item) + " is not in the data base"
+        print>>file,str(item) + " is not in the database"
